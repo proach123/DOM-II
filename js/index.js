94adb9ec6e5e1e70ca20ddf5ba32c9edf6c56ed9
Patrick Roach <patrickevanroach@gmail.com>
2019-12-03 15:41:43 -0700

    added first event

--- js/index.js
+++ js/index.js
@@ -1,1 +1,16 @@
 // Your code goes here
+
+const divButtons = document.querySelectorAll('.btn')
+
+console.log(divButtons)
+
+divButtons.forEach((button)=>{
+    button.addEventListener('click', (e)=>{e.target.style.backgroundColor = 'blue'})
+})
+
+
+const logo = document.querySelector('h1')
+
+logo.addEventListener("mouseover", (e)=>{e.target});
+
+
